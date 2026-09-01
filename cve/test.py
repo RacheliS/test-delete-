56e3bb36598c2255858a354ef91f9e6cfe5b54d2
Racheli Amrusi <Racheli.Sulemani@exscientia.co.il>
2026-09-01 09:11:08 +0300

    Add function to retrieve user profile from database

---
+++ cve/test.py
@@ -0,0 +1,10 @@
+import sqlite3
+
+def get_user_profile(username):
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+    
+    query = f"SELECT * FROM users WHERE username = '{username}'"
+    
+    cursor.execute(query)
+    return cursor.fetchall()
